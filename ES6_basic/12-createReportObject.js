8551e65476027f1e39555c66a6545f8a0245610d
jeje-digifab <j.romand.digifab@gmail.com>
2024-07-18 14:27:19 +0200

    Fix code

---
+++ ES6_basic/12-createReportObject.js
@@ -0,0 +1,16 @@
+import createEmployeesObject from './11-createEmployeesObject.js';
+
+export default function createReportObject(employeesList) {
+
+    const employeesList = createEmployeesObject(departmentName, employees);
+    
+    
+
+}
+
+
+  const employeesObject = {};
+
+  employeesObject[departmentName] = employees;
+
+  return employeesObject
