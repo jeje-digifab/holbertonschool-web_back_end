576f4da721e490d6603595bea4fdbb0b122e24c8
jeje-digifab <j.romand.digifab@gmail.com>
2024-11-16 13:20:18 +0100

    Fix task 5

--- ES6_basic/5-spread-operator.js
+++ ES6_basic/5-spread-operator.js
@@ -1,9 +1,3 @@
 export default function concatArrays(array1, array2, string) {
-  const concatenatedArray = array1.concat(array2);
-
-  for (let i = 0; i < string.length; i += 1) {
-    concatenatedArray.push(string[i]);
-  }
-
-  return concatenatedArray;
+  return [...array1, ...array2, ...string];
 }
