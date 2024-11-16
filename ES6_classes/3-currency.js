1aca88f2a67b21c8f268eb4316042b96478db209
jeje-digifab <j.romand.digifab@gmail.com>
2024-11-16 13:07:07 +0100

    Fix task 3

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -1,5 +1,12 @@
 class Currency {
   constructor(code, name) {
+    if (typeof code !== 'string') {
+      throw new TypeError('code must be a string');
+    }
+    if (typeof name !== 'string') {
+      throw new TypeError('name must be a string');
+    }
+
     this._code = code;
     this._name = name;
   }
@@ -14,14 +21,14 @@ class Currency {
 
   set code(newCode) {
     if (typeof newCode !== 'string') {
-      throw new TypeError('newCode must be a string');
+      throw new TypeError('code must be a string');
     }
     this._code = newCode;
   }
 
   set name(newName) {
     if (typeof newName !== 'string') {
-      throw new TypeError('Name must be a string');
+      throw new TypeError('name must be a string');
     }
     this._name = newName;
   }
