6476f7e3f6d7ce548d07d6d7379fe9b84fe850e4
jeje-digifab <j.romand.digifab@gmail.com>
2024-11-16 12:59:14 +0100

    Fix task 5

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,11 +1,8 @@
 class Building {
   constructor(sqft) {
-    if (new.target === Building) {
-      throw new Error('Cannot instantiate abstract class Building');
-    }
     this._sqft = sqft;
 
-    if (typeof this.evacuationWarningMessage !== 'function') {
+    if (new.target !== Building && typeof this.evacuationWarningMessage !== 'function') {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
   }
