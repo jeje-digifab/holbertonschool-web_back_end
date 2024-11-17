9a6d9c9b17cc1e895fdcebfcffc9927d782f4dac
jeje-digifab <j.romand.digifab@gmail.com>
2024-11-17 12:22:01 +0100

    Fix task 7

--- ES6_classes/7-airport.js
+++ ES6_classes/7-airport.js
@@ -5,11 +5,11 @@ class Airport {
   }
 
   toString() {
-    return `[${this._code}]`;
+    return `[object ${this._code}]`;
   }
 
   [Symbol.for('nodejs.util.inspect.custom')]() {
-    return `Airport ${this.toString()} { _name: '${this._name}', _code: '${this._code}' }`;
+    return `Airport [${this._code}] { _name: '${this._name}', _code: '${this._code}' }`;
   }
 }
 
