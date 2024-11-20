0f4fb140e2067de3521bd6dc54526f36411a5024
jeje-digifab <j.romand.digifab@gmail.com>
2024-11-20 11:44:42 +0100

    Fix task 3

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -6,8 +6,20 @@ import {
 export default function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()])
     .then((responses) => {
-      const [photoResponse, userResponse] = responses;
-      console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
+      const photoResponse = responses[0];
+      const userResponse = responses[1];
+
+      if (
+        photoResponse
+        && photoResponse.body
+        && userResponse
+        && userResponse.firstName
+        && userResponse.lastName
+      ) {
+        console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
+      } else {
+        console.log('Signup system offline');
+      }
     })
     .catch(() => {
       console.log('Signup system offline');
