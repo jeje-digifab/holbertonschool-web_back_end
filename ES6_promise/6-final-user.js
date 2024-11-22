8afe1cd6a4005ec726f4bd5c7cbf08b05e36cfb5
jeje-digifab <j.romand.digifab@gmail.com>
2024-11-22 11:15:54 +0100

    Fix task 6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -5,5 +5,9 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   const signUpPromise = signUpUser(firstName, lastName);
   const uploadPhotoPromise = uploadPhoto(fileName);
 
-  return Promise.allSettled([signUpPromise, uploadPhotoPromise]);
+  return Promise.allSettled([signUpPromise, uploadPhotoPromise])
+    .then((results) => results.map((result) => ({
+      status: result.status,
+      value: result.status === 'fulfilled' ? result.value : result.reason,
+    })));
 }
