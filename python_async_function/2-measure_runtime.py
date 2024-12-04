0ae3f21ae029933c451b9b48b057f0d3a759635a
jeje-digifab <j.romand.digifab@gmail.com>
2024-12-04 16:42:44 +0100

    Fix requirement

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -9,7 +9,17 @@ import time
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-async def _measure_time(n: int, max_delay: int) -> float:
+async def measure_time_async(n: int, max_delay: int) -> float:
+    """
+    Asynchronously measure the total execution time for wait_n(n, max_delay).
+
+    Args:
+        n (int): The number of times to call wait_n.
+        max_delay (int): The maximum delay for each call to wait_n.
+
+    Returns:
+        float: The average time per call to wait_n.
+    """
     start_time: float = time.time()
     await wait_n(n, max_delay)
     end_time: float = time.time()
@@ -18,4 +28,14 @@ async def _measure_time(n: int, max_delay: int) -> float:
 
 
 def measure_time(n: int, max_delay: int) -> float:
-    return asyncio.run(_measure_time(n, max_delay))
+    """
+    Synchronously measure the total execution time for wait_n(n, max_delay).
+
+    Args:
+        n (int): The number of times to call wait_n.
+        max_delay (int): The maximum delay for each call to wait_n.
+
+    Returns:
+        float: The average time per call to wait_n.
+    """
+    return asyncio.run(measure_time_async(n, max_delay))
