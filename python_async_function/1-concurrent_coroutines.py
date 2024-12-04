21ce77adede33b2952444cb5d22652d9bc2e1426
jeje-digifab <j.romand.digifab@gmail.com>
2024-12-04 13:09:37 +0100

    Fix annotations

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -4,10 +4,12 @@ This script defines an asynchronous coroutine that waits for a random delay.
 """
 
 import asyncio
+from typing import List
+
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     Waits for a random delay `n` times and returns a sorted list of delays.
 
@@ -15,7 +17,7 @@ async def wait_n(n: int, max_delay: int) -> list[float]:
     :param max_delay: Maximum delay for each wait.
     :return: Sorted list of delays.
     """
-    delays: list[float] = []
+    delays: List[float] = []
     tasks = [wait_random(max_delay) for _ in range(n)]
     for task in asyncio.as_completed(tasks):
         delay = await task
