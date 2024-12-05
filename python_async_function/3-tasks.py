49a6d3667d922caed2a92d0806bdb6713d4eff22
jeje-digifab <j.romand.digifab@gmail.com>
2024-12-05 14:04:55 +0100

    Fix annotations task 3

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -6,11 +6,10 @@ This script defines an asynchronous coroutine that waits for a random delay.
 import asyncio
 from typing import Any
 
-
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int) -> Any:
+def task_wait_random(max_delay: int) -> asyncio.Task:
     """
     Create an asyncio Task that waits for a random delay.
 
@@ -24,5 +23,7 @@ def task_wait_random(max_delay: int) -> Any:
         asyncio.Task: The created asyncio Task.
     """
     coroutine = wait_random(max_delay)
+
     my_task = asyncio.create_task(coroutine)
+
     return my_task
