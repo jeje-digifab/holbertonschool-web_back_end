45ffa5861775f41127a11900b6af09f149253ac5
jeje-digifab <j.romand.digifab@gmail.com>
2024-12-13 12:53:04 +0100

    Fix task 0

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -5,10 +5,10 @@ This script defines an asynchronous generator that waits for a random delay.
 
 import random
 import asyncio
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """
     An asynchronous generator that yields random float values between 0 and 10.
 
