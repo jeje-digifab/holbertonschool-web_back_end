a1e42378545fb40b5b33dd183de1dea0b8d2c884
jeje-digifab <j.romand.digifab@gmail.com>
2024-12-13 20:25:07 +0100

    Add task 6 to 8

---
+++ NoSQL/8-all.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Write a Python function that lists all documents in a collection
+"""
+
+
+def list_all(mongo_collection):
+    """
+    List all documents in a MongoDB collection.
+
+    Args:
+    mongo_collection (pymongo.collection.Collection): The MongoDB collection to list documents from.
+
+    Returns:
+    list: A list of all documents in the collection.
+    """
+    return list(mongo_collection.find())
