3294e2e6b35b1f830227fb46c030b23ee500e2fc
jeje-digifab <j.romand.digifab@gmail.com>
2024-12-13 23:27:27 +0100

    Fix task 12

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -7,18 +7,28 @@ stored in MongoDB
 
 from pymongo import MongoClient
 
-client = MongoClient('mongodb://127.0.0.1:27017')
-collection = client.logs.nginx
+if __name__ == "__main__":
+    """
+    Main entry point of the script.
 
+    This block of code is executed when the script is run directly.
+    It performs the following tasks:
+    1. Connects to the MongoDB instance.
+    2. Accesses the 'nginx' collection in the 'logs' database.
+    3. Counts and prints the total number of logs.
+    4. Counts and prints the number of logs for each HTTP method.
+    5. Counts and prints the number of status check logs.
+    """
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    collection = client.logs.nginx
 
-print("{} logs".format(collection.count_documents({})))
+    print("{} logs".format(collection.count_documents({})))
+    print("Methods:")
 
-print("Methods:")
-methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-for method in methods:
-    count = collection.count_documents({"method": method})
-    print("\tmethod {}: {}".format(method, count))
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        print("    method {}: {}".format(
+            method, collection.count_documents({"method": method})))
 
-
-print("{} status check".format(collection.count_documents(
-    {"method": "GET", "path": "/status"})))
+    print("{} status check".format(collection.count_documents(
+        {"method": "GET", "path": "/status"})))
