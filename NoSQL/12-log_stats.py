088f86677a751cec93d06e35b45431e53219c32d
jeje-digifab <j.romand.digifab@gmail.com>
2024-12-17 13:19:17 +0100

    Fix task 12

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,9 +1,31 @@
 #!/usr/bin/env python3
 """
-Write a Python script that provides some stats about Nginx logs
-stored in MongoDB
-"""
+This Python script connects to a MongoDB database and provides statistics
+about Nginx logs stored in the 'nginx' collection of the 'logs' database.
 
+The script performs the following tasks:
+1. Connects to the MongoDB instance running on localhost at port 27017.
+2. Accesses the 'nginx' collection in the 'logs' database.
+3. Counts the total number of logs in the collection.
+4. Counts the number of logs for each HTTP method (GET, POST, PUT, PATCH,
+    DELETE).
+5. Counts the number of status check logs (GET requests to the '/status' path).
+6. Prints the statistics to the console.
+
+Usage:
+Run the script from the command line to see the statistics printed
+to the console.
+
+Example output:
+0 logs
+Methods:
+    method GET: 0
+    method POST: 0
+    method PUT: 0
+    method PATCH: 0
+    method DELETE: 0
+0 status check
+"""
 
 from pymongo import MongoClient
 
@@ -22,7 +44,8 @@ if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
     collection = client.logs.nginx
 
-    print("{} logs".format(collection.count_documents({})))
+    total_logs = collection.count_documents({})
+    print("{} logs".format(total_logs))
     print("Methods:")
 
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
@@ -30,5 +53,6 @@ if __name__ == "__main__":
         print("    method {}: {}".format(
             method, collection.count_documents({"method": method})))
 
-    print("{} status check".format(collection.count_documents(
-        {"method": "GET", "path": "/status"})))
+    status_check_count = collection.count_documents(
+        {"method": "GET", "path": "/status"})
+    print("{} status check".format(status_check_count))
