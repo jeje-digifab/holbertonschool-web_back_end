70b54e18ed619e44e0fdde5bd27256f9881adadd
jeje-digifab <j.romand.digifab@gmail.com>
2025-01-20 17:04:39 +0100

    Fix task 2

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,28 +1,19 @@
 const fs = require('fs');
-const readline = require('readline');
 
 function countStudents(file) {
-  const fileStream = fs.createReadStream(file);
+  try {
+    const data = fs.readFileSync(file, 'utf8');
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
 
-  const rl = readline.createInterface({
-    input: fileStream,
-    crlfDelay: Infinity,
-  });
+    const headers = lines[0].split(',');
+    let studentCount = 0;
+    let csStudentCount = 0;
+    const csStudents = [];
+    let sweStudentCount = 0;
+    const sweStudents = [];
 
-  let headers = [];
-  let headerProcessed = false;
-  let studentCount = 0;
-  let csStudentCount = 0;
-  const csStudents = [];
-  let sweStudentCount = 0;
-  const sweStudents = [];
-
-  rl.on('line', (line) => {
-    if (!headerProcessed) {
-      headers = line.split(',');
-      headerProcessed = true;
-    } else {
-      const row = line.split(',');
+    for (let i = 1; i < lines.length; i += 1) {
+      const row = lines[i].split(',');
       const rowObject = {};
       headers.forEach((header, index) => {
         rowObject[header.trim()] = row[index].trim();
@@ -37,17 +28,13 @@ function countStudents(file) {
         sweStudents.push(rowObject.firstname);
       }
     }
-  });
 
-  rl.on('close', () => {
     console.log(`Number of students: ${studentCount}`);
     console.log(`Number of students in CS: ${csStudentCount}. List: ${csStudents.join(', ')}`);
     console.log(`Number of students in SWE: ${sweStudentCount}. List: ${sweStudents.join(', ')}`);
-  });
-
-  rl.on('error', () => {
+  } catch (error) {
     throw new Error('Cannot load the database');
-  });
+  }
 }
 
 module.exports = countStudents;
