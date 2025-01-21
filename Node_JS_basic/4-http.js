d1199ff574bfe76f7d5c7de4dbeccd8221359c23
jeje-digifab <j.romand.digifab@gmail.com>
2025-01-21 11:24:39 +0100

    Add task 4

---
+++ Node_JS_basic/4-http.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.write('Hello Holberton School!');
+  res.end();
+});
+
+server.listen(1245);
+
+module.exports = server;
