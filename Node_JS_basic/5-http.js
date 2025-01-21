f95325120ee794d2247e9ac0446ba9ba10a5d5d4
jeje-digifab <j.romand.digifab@gmail.com>
2025-01-21 17:32:30 +0100

    Fix task 5

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -3,45 +3,48 @@ const url = require('url');
 const countStudents = require('./3-read_file_async');
 
 const app = http.createServer((req, res) => {
-    const parsedUrl = url.parse(req.url, true);
-    const {
-        pathname
-    } = parsedUrl;
+  const parsedUrl = url.parse(req.url, true);
+  const { pathname } = parsedUrl;
 
-    if (pathname === '/students') {
-        if (process.argv.length > 2) {
-            const csvFile = process.argv[2];
-            const output = 'This is the list of our students\n';
+  if (pathname === '/') {
+    res.writeHead(200, {
+      'Content-Type': 'text/plain',
+    });
+    res.end('Hello Holberton School!');
+  } else if (pathname === '/students') {
+    if (process.argv.length > 2) {
+      const csvFile = process.argv[2];
+      const output = 'This is the list of our students\n';
 
-            const originalLog = console.log;
-            let capturedOutput = '';
-            console.log = (message) => {
-                capturedOutput += `${message}\n`;
-            };
+      const originalLog = console.log;
+      let capturedOutput = '';
+      console.log = (message) => {
+        capturedOutput += `${message}\n`;
+      };
 
-            countStudents(csvFile)
-                .then(() => {
-                    console.log = originalLog;
+      countStudents(csvFile)
+        .then(() => {
+          console.log = originalLog;
 
-                    res.writeHead(200, {
-                        'Content-Type': 'text/plain',
-                    });
-                    res.write(output);
-                    res.write(capturedOutput.trim());
-                    res.end();
-                });
-        } else {
-            res.writeHead(200, {
-                'Content-Type': 'text/plain',
-            });
-            res.end('Hello Holberton School!');
-        }
-    } else {
-        res.writeHead(200, {
+          res.writeHead(200, {
             'Content-Type': 'text/plain',
+          });
+          res.write(output);
+          res.write(capturedOutput.trim());
+          res.end();
         });
-        res.end('Hello Holberton School!');
+    } else {
+      res.writeHead(200, {
+        'Content-Type': 'text/plain',
+      });
+      res.end('Hello Holberton School!');
     }
+  } else {
+    res.writeHead(200, {
+      'Content-Type': 'text/plain',
+    });
+    res.end('Hello Holberton School!');
+  }
 });
 
 app.listen(1245);
