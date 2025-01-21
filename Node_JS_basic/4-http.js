4b8e8c8123a62cb26f8ce91a31f4e40c273ebab0
jeje-digifab <j.romand.digifab@gmail.com>
2025-01-21 16:31:44 +0100

    Modify code task 4

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,10 +1,12 @@
 const http = require('http');
 
-const server = http.createServer((req, res) => {
-  res.write('Hello Holberton School!');
-  res.end();
+const app = http.createServer((req, res) => {
+  res.writeHead(200, {
+    'Content-Type': 'text/plain',
+  });
+  res.end('Hello Holberton School!');
 });
 
-server.listen(1245);
+app.listen(1245);
 
-module.exports = server;
+module.exports = app;
